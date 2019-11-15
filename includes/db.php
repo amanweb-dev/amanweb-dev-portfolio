82f5d1e9d844eadee4a29580fc63982b44eeacde
Amanullah Aman <amanullah.bcse@gmail.com>
2019-11-15 18:37:23 +0600

    my first web

--- includes/db.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//Connect to The DAtabase
-
-$connection = mysqli_connect("localhost","root","","kenabecha");
-if (!$connection) {
-	die("database Connection failed ".mysqli_error($connection));
-
-}
- ?>
--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php session_start(); ?>
-
-<?php 
-
-$_SESSION['user_email'] = null; 
-$_SESSION['user_pass'] =null; 
-header("Location: ads.php" );
-
- ?>
